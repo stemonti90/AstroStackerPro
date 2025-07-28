ee77a4d2f6bef63a7af7feb7ec4347e6cce0ffcf
Stefano Monticciolo <155195751+stemonti90@users.noreply.github.com>
2025-07-28 10:08:11 +0200

    Delete AstroStackerPro directory

--- AstroStackerPro/Sources/Core/EditorViewModel.swift
+++
@@ -1,5 +0,0 @@
-
-import Foundation
-final class EditorViewModel: ObservableObject {
-    // TODO: stato immagine e editing non distruttivo
-}
--- AstroStackerPro/Sources/Core/PlannerViewModel.swift
+++
@@ -1,6 +0,0 @@
-
-import Foundation
-final class PlannerViewModel: ObservableObject {
-    // TODO: integrazione SwiftAA, OpenWeather, Light Pollution Map
-    @Published var moonPhase: Double = 0
-}
